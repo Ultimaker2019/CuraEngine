1f1d2b920bdb53e4c0b892d7466b9dfe1a1e0c1a
edwardfang1945 <edwardfang1945@outlook.com>
2020-08-14 13:36:25 +0800

    Remove sparse tiny blobs

--- src/skin.cpp
+++ src/skin.cpp
@@ -112,6 +112,15 @@ void generateSparse(int layerNr, SliceVolumeStorage& storage, int extrusionWidth
                 i -= 1;
             }
         }
+        for(unsigned int i=0; i<sparse.size(); i++)
+        {
+            double area = INT2MM(INT2MM(fabs(sparse[i].area())));
+            if (area < minAreaSize) /* Only create an up/down skin if the area is large enough. So you do not create tiny blobs of "trying to fill" */
+            {
+                sparse.remove(i);
+                i -= 1;
+            }
+        }
         
         part->sparseOutline = sparse.difference(result);
     }
