1f549ba5a3ff015a84f230549eb2eaf344bc5438
edwardfang1945 <edwardfang1945@outlook.com>
2020-08-26 00:20:23 +0800

    Add cmake and make batch in window platform

---
+++ win_clean.bat
@@ -0,0 +1,3 @@
+echo off
+if exist build\ rmdir /q /s build\
+if exist cmake_build\ rmdir /q /s cmake_build\
---
+++ win_make.bat
@@ -0,0 +1,15 @@
+echo off
+set COMPILER_PATH=C:\Users\edwar\OneDrive\compiler_tools
+set PATH=C:\Program Files\CMake\bin;
+set PATH=C:\Program Files (x86)\NSIS;%PATH%
+set PATH=C:\Program Files\Git\cmd;%PATH%
+set PATH=%COMPILER_PATH%\mingw32\msys\1.0\bin;%PATH%
+set PATH=%COMPILER_PATH%\cygwin64\bin;%PATH%
+set PATH=%COMPILER_PATH%\mingw-w64\i686-7.3.0-posix-dwarf-rt_v5-rev0\mingw32\bin;%PATH%
+set PATH=C:\Program Files\7-Zip;%PATH%
+set PATH=%COMPILER_PATH%\curl\curl-7.71.1-win32-mingw\bin;%PATH%
+
+SET VERSION=%date:~0,4%%date:~5,2%%date:~8,2%
+REM %time:~0,2%%time:~3,2%%time:~6,2%
+mingw32-make -C ./ DEFINES="'VERSION=\"%VERSION%\"'"
+pause
