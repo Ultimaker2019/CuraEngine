c353d1ea1061158922c94210670e13ca33e8980e
edwardfang2018 <edwardfang2018@outlook.com>
2023-11-04 23:20:54 +0800

    drug test

--- src/LayerPlan.cpp
+++ src/LayerPlan.cpp
@@ -1594,7 +1594,10 @@ void LayerPlan::writeGCode(GCodeExport& gcode)
                 gcode.writeRetraction(retraction_config);
                 if (path.perform_z_hop)
                 {
-                    gcode.writeZhopStart(z_hop_height);
+                    if (path_idx != 0 && layer_nr == 0)
+                    {
+                        gcode.writeZhopStart(z_hop_height);
+                    }
                     z_hop_height = retraction_config.zHop; // back to normal z hop
                 }
                 else
